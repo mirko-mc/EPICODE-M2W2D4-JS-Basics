78d92346d2c8e1f91c4529777eb38b340d4a76e1
DarkSide Labs <8547740+DarkSideLabs@users.noreply.github.com>
2024-03-22 21:45:48 +0100

    progetto completato in duplice modo

--- assets/js/S2P2-scaricabile.js
+++ assets/js/S2P2-scaricabile.js
@@ -17,23 +17,93 @@ Infine, crea un SECONDO array in cui inserirai SOLO gli ambassador.
 */
 
 const marco = {
-  name: "Marco",
-  lastName: "Rossi",
-  isAmbassador: true,
+    name: "Marco",
+    lastName: "Rossi",
+    isAmbassador: true,
 }
 
 const paul = {
-  name: "Paul",
-  lastName: "Flynn",
-  isAmbassador: false,
+    name: "Paul",
+    lastName: "Flynn",
+    isAmbassador: false,
 }
 
 const amy = {
-  name: "Amy",
-  lastName: "Reed",
-  isAmbassador: false,
+    name: "Amy",
+    lastName: "Reed",
+    isAmbassador: false,
 }
 
-const prices = [34, 5, 2]
+const prices = [34, 55, 20]
 const shippingCost = 50
-let utenteCheEffettuaLAcquisto = amy //cambia il valore qui per provare se il tuo algoritmo funziona!
+let utenteCheEffettuaLAcquisto = marco //cambia il valore qui per provare se il tuo algoritmo funziona!
+
+// prezzo totale del carrello
+let totaleCarrello = 0;
+for (const ITERATOR of prices) {
+    totaleCarrello += ITERATOR;
+}
+console.log(`TOTALE CARRELLO -> ${totaleCarrello}`);
+// se l'utente è ambassador, sconto 30% sul carrello
+if (utenteCheEffettuaLAcquisto.isAmbassador) totaleCarrello -= totaleCarrello * 0.3;
+console.log(`TOTALE CARRELLO SCONTATO DEL 30% -> ${parseFloat(totaleCarrello.toFixed(2))}`);
+/*
+Se l'utente ha la proprietà "isAmbassador" con valore false, il carrello NON deve venire scontato.
+In entrambi i casi, la spedizione è gratuita per ogni carrello con costo superiore a 100. Altrimenti, aggiungi il valore fornito per coprire il costo della spedizione.
+*/
+totaleCarrello < 100 ? totaleCarrello += shippingCost : totaleCarrello = totaleCarrello;
+console.log(`TOTALE CARRELLO CONTEGGIATA LA SPEDIZIONE -> ${parseFloat(totaleCarrello.toFixed(2))}`);
+
+/*
+In basso troverai degli esempi di utenti, una lista prezzi e un costo fisso di spedizione.
+Crea un array di utenti (usando .push) e stampa, per ogni utente (quindi con un loop) la frase "NOMEUTENTE COGNOMEUTENTE e' / non e' un ambassador" basandoti sui dati contenuti nell'oggetto. 
+ES. L'utente Marco Rossi e' un ambassador, quindi la frase dovrebbe essere "Marco Rossi e' un ambassador"
+Infine, crea un SECONDO array in cui inserirai SOLO gli ambassador.
+*/
+let utenti = [];
+utenti.push(paul, amy, marco);
+for (const ITERATOR of utenti) {
+    console.log(`${ITERATOR.name} ${ITERATOR.lastName} ${ITERATOR.isAmbassador ? 'è un Ambassador.' : 'non è un Ambassador.'} `)
+}
+let ambassador = [];
+for (const ITERATOR of utenti) {
+    if (ITERATOR.isAmbassador) ambassador.push(ITERATOR);
+}
+console.log("UTENTI AMBASSADOR -> ", ambassador);
+for (const iterator of utenti) {
+    iterator.isAmbassador
+}
+
+
+
+
+// ------------ MIA INIZIATIVA
+let utenti2 = [];
+let ambassador2 = [];
+utenti2.push(marco, paul, amy);
+for (const ITERATOR of utenti2) {
+    let totaleCarrello = 0;
+    for (const ITERATOR of prices) {
+        totaleCarrello += ITERATOR;
+    }
+    if (ITERATOR.isAmbassador) {
+        console.log(`
+        ${ITERATOR.name} ${ITERATOR.lastName} E' Ambassador
+        Totale carrello -> ${totaleCarrello}
+        Totale carrello scontato del 30% -> ${parseFloat((totaleCarrello -= totaleCarrello * .3).toFixed(2))}
+        Totale carrello compreso più spedizione -> ${parseFloat((totaleCarrello + shippingCost).toFixed(2))}
+        `);
+        ambassador2.push(ITERATOR);
+    }
+    else {
+        console.log(`
+        ${ITERATOR.name} ${ITERATOR.lastName} NON E' Ambassador
+        Totale carrello senza sconto -> ${parseFloat(totaleCarrello.toFixed(2))}
+        Totale carrello compreso più spedizione -> ${parseFloat((totaleCarrello + shippingCost).toFixed(2))}
+        `);
+    }
+}
+console.log("UTENTI AMBASSADOR -> ", ambassador2);
+for (const iterator of utenti2) {
+    iterator.isAmbassador
+}
